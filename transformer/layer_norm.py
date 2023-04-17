2e56bce109e1f16e5b844cbe27a1924e2dde5636
Frank Hua <fh422@ic.ac.uk>
2023-04-17 15:17:27 +0100

    Add multiple files of transformer sub components

---
+++ transformer/layer_norm.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+
+
+class LayerNorm(nn.Module):
+    def __init__(self, d_model, eps=1e-12):
+        super(LayerNorm, self).__init__()
+        self.gamma = torch.ones(d_model, requires_grad=True)
+        self.beta = torch.zeros(d_model, requires_grad=True)
+        self.eps = eps
+
+    def forward(self, x):
+        mean = x.mean(-1, keepdim=True)
+        var = x.var(-1, unbiased=False, keepdim=True)
+        # '-1' means last dimension.
+
+        out = (x - mean) / torch.sqrt(var + self.eps)
+        out = self.gamma * out + self.beta
+        return out
---
+++ transformer/positional_encoding.py
@@ -0,0 +1,43 @@
+import torch
+import torch.nn as nn
+
+
+class PositionalEncoding(nn.Module):
+    """
+    compute sinusoid encoding.
+    """
+
+    def __init__(self, model_dim, max_len, device):
+        """
+        constructor of sinusoid encoding class
+        :param model_dim: dimension of model
+        :param max_len: max sequence length
+        :param device: hardware device setting
+        """
+        super(PositionalEncoding, self).__init__()
+
+        # same size with input matrix (for adding with input matrix)
+        self.encoding = torch.zeros(max_len, model_dim, device=device)
+        self.encoding.requires_grad = False  # we don't need to compute gradient
+
+        pos = torch.arange(0, max_len, device=device)
+        pos = pos.float().unsqueeze(dim=1)
+        # 1D => 2D unsqueeze to represent word's position
+
+        _2i = torch.arange(0, model_dim, step=2, device=device).float()
+        # 'i' means index of model_dim (e.g. embedding size = 50, 'i' = [0,50])
+        # "step=2" means 'i' multiplied with two (same with 2 * i)
+
+        self.encoding[:, 0::2] = torch.sin(pos / (10000 ** (_2i / model_dim)))
+        self.encoding[:, 1::2] = torch.cos(pos / (10000 ** (_2i / model_dim)))
+        # compute positional encoding to consider positional information of words
+
+    def forward(self, x):
+        # self.encoding
+        # [max_len = 512, model_dim = 512]
+
+        batch_size, seq_len = x.size()
+        # [batch_size = 128, seq_len = 30]
+
+        return self.encoding[:seq_len, :]
+        # [seq_len = 30, model_dim = 512]
