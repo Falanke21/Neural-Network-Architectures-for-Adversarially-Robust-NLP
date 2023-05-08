476d2072cd42d3d9685fb3cfeeab2bef7a55a1e5
Frank Hua <fh422@ic.ac.uk>
2023-05-08 23:39:28 +0100

    Add newest training result and config

--- config/transformer_config.py
+++ config/transformer_config.py
@@ -1,14 +1,14 @@
 class TransformerConfig:
     USE_GPU = True
-    NUM_EPOCHS = 5
+    NUM_EPOCHS = 8
     TRAIN_SEQ_LENGTH = 150
-    TEST_SEQ_LENGTH = 450
+    TEST_SEQ_LENGTH = 150
     BATCH_SIZE = 200
-    LEARNING_RATE = 0.001
+    LEARNING_RATE = 1e-4
 
     BETAS = (0.9, 0.98)  # same as original paper
     ADAM_EPSILON = 1e-9  # same as original paper
-    WEIGHT_DECAY = 0  # 0 means no weight decay
+    WEIGHT_DECAY = 1e-6  # 0 means no weight decay
     GRADIENT_CLIP = True
     GRADIENT_CLIP_VALUE = 1
     UPSAMPLE_NEGATIVE = True
@@ -16,7 +16,8 @@ class TransformerConfig:
     LABEL_SMOOTHING = False  # label smoothing lead to worse results
     LABEL_SMOOTHING_EPSILON = 0.1  # 0 means no smoothing
 
+    NUM_LAYERS = 4
     D_MODEL = 300
     FFN_HIDDEN = 1024
     N_HEAD = 5
-    DROPOUT = 0.1
+    DROPOUT = 0
