23ac43f26859247f0e2b525bffb12b11e303343a
Frank Hua <fh422@ic.ac.uk>
2023-06-16 16:57:16 +0100

    Add glove example code

---
+++ utils/glove_examples.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+import torchtext
+import pickle
+
+glove = torchtext.vocab.GloVe(name='6B', dim=300, cache='/vol/bitbucket/fh422/torchtext_cache')
+print("Loaded glove {} words".format(len(glove)))
+
+with open('../data/vocab200k.pkl', 'rb') as f:
+    vocab = pickle.load(f)
+print("Loaded vocab {} words".format(len(vocab)))
+
+temp = ['you', 'are', 'an', 'idiot']
+# get the indices of the words
+vocab_indices = [vocab.get_stoi()[word] for word in temp]
+glove_indices = [glove.get_stoi()[word] for word in temp]
+print("Vocab indices: {}".format(vocab_indices))
+print("Glove indices: {}".format(glove_indices))
