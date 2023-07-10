3b261f85c2f6e224ec8511c60f40c30260a23844
Frank Hua <fh422@ic.ac.uk>
2023-07-10 12:45:05 +0100

    Delete Wa Ua weight matrices in additive attention
    Ablation experiment showed that they have no effect over training

--- transformer/additive_attention.py
+++ transformer/additive_attention.py
@@ -18,8 +18,6 @@ class AdditiveAttention(nn.Module):
 
     def __init__(self, d_tensor):
         super(AdditiveAttention, self).__init__()
-        self.Wa = nn.Linear(d_tensor, d_tensor)
-        self.Ua = nn.Linear(d_tensor, d_tensor)
         self.va = nn.Linear(d_tensor, 1)
         self.softmax = nn.Softmax(dim=-1)
         print("Using Additive Attention")
@@ -30,10 +28,7 @@ class AdditiveAttention(nn.Module):
 
         # 1. calculate alignment model output tensor e
         # [batch_size, head, length, d_tensor]
-        # e = torch.tanh(self.Wa(q) + self.Ua(k))
-        e1 = self.Wa(q)  # [batch_size, head, length, d_tensor]
-        e2 = self.Ua(k)  # [batch_size, head, length, d_tensor]
-        e3 = e1 + e2
+        e3 = q + k  # [batch_size, head, length, d_tensor]
         e = torch.tanh(e3)
         e = self.va(e).squeeze(-1)  # [batch_size, head, length]
 
