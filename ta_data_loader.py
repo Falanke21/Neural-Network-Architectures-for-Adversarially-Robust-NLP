725b58359a463df7725696da92dccb5a631adfe4
Frank Hua <fh422@ic.ac.uk>
2023-08-01 16:45:53 +0100

    Delete unnecessary code from ta_data_loader.py

--- ta_data_loader.py
+++ ta_data_loader.py
@@ -1,5 +1,4 @@
 # Custom dataset loader for textattack
-import numpy as np
 import pandas as pd
 
 import textattack
@@ -7,10 +6,6 @@ import textattack
 # Load a dataset
 data_path = "data/data300k-with-3stars/test.csv"
 df = pd.read_csv(data_path)
-# there are some rows with label = 'label', we need to remove them
-df = df[df['label'] != 'label']
-# convert label to int
-df['label'] = df['label'].astype(int)
 
 # transform df into a list of tuples [(text, label), ...]
 data = list(zip(df['text'].tolist(), df['label'].tolist()))
